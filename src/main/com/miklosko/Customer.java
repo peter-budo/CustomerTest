54a02b70a1537ef784cd047f77b8c56a3d1240f4
Peter <peter.budo@gmail.com>
2012-04-25 18:10:28 +0100

    Introduced Logger to replace system output calls (would need appropriate config file) Replaced String result with StringBuilder and moved amount and frequency points inside try statement

--- src/main/com/miklosko/Customer.java
+++ src/main/com/miklosko/Customer.java
@@ -1,32 +1,37 @@
 package com.miklosko;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Enumeration;
 import java.util.Vector;
 
-class Customer extends DomainObject
-{
+class Customer extends DomainObject {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Customer.class);
     private String _name;
     private Vector<Rental> _rentals = new Vector<Rental>();
 
     public Customer(String name) {
         _name = name;
     }
+
     public String statement() {
         Enumeration rentals = _rentals.elements();
-        String result = "Rental Record for " + name() + "\n";
+        StringBuilder result = new StringBuilder("Rental Record for " + name() + "\n");
         try {
             while (rentals.hasMoreElements()) {
 
                 Rental rental = (Rental) rentals.nextElement();
-                result += "\t" + rental.getTitle()+ "\t" + String.valueOf(rental.charge()) + "\n";
+                result.append("\t" + rental.getTitle() + "\t" + String.valueOf(rental.charge()) + "\n");
             }
+            result.append("Amount owed is " + String.valueOf(totalAmount()) + "\n");
+            result.append("You earned " + String.valueOf(frequentRenterPoints()) + " frequent renter points");
         } catch (Throwable t) {
-            System.out.println("Unexpected exception " + t);
+            LOG.error("Unexpected exception " + t);
         }
-        //add footer lines
-        result +=  "Amount owed is " + String.valueOf(totalAmount()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints()) + " frequent renter points";
-        return result;
+
+        return result.toString();
 
     }
 
@@ -34,30 +39,30 @@ class Customer extends DomainObject
         return _name;
     }
 
-    public void addRental(Rental rental){
+    public void addRental(Rental rental) {
         _rentals.add(rental);
     }
 
-    private int frequentRenterPointsFor(Rental rental){
+    private int frequentRenterPointsFor(Rental rental) {
         return rental.frequentRenterPoints();
     }
 
-    private double totalAmount(){
+    private double totalAmount() {
         double totalAmount = 0;
         Enumeration rentals = _rentals.elements();
-        while (rentals.hasMoreElements()){
+        while (rentals.hasMoreElements()) {
             Rental rental = (Rental) rentals.nextElement();
-            totalAmount  += rental.charge();
+            totalAmount += rental.charge();
         }
         return totalAmount;
     }
 
-    private int frequentRenterPoints(){
+    private int frequentRenterPoints() {
         int points = 0;
         Enumeration rentals = _rentals.elements();
-        while (rentals.hasMoreElements()){
+        while (rentals.hasMoreElements()) {
             Rental rental = (Rental) rentals.nextElement();
-            points  += frequentRenterPointsFor(rental);
+            points += frequentRenterPointsFor(rental);
         }
         return points;
     }
